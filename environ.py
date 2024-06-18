aab49be5bb08e05c8498b7fd6778f0b6d90cae22
sunsolyee <gothf316@kookmin.ac.kr>
2024-06-19 03:08:02 +0900

    test 2

--- environ.py
+++ environ.py
@@ -4,7 +4,11 @@ def application(environ, start_response):
     response_body = [
         '%s: %s' % (key, value) for key, value in sorted(environ.items())
     ]
+<<<<<<< HEAD
     response_body = '\r\n'.join(response_body)
+=======
+    response_body = '\n\n'.join(response_body)
+>>>>>>> 0d84b41 (test 1)
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'text/plain'),
